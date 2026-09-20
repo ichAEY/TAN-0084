f5cd89fc216681c1f7ea6dff1c1d4c1e70be5aa5
tanem <melikyan.mac@gmail.com>
2026-09-20 05:40:08 +0300

    Use client-site template tests for TAN-0084

--- tests/template.test.mjs
+++ tests/template.test.mjs
@@ -19,15 +19,6 @@ test("static export builds from the empty template", () => {
   assert.match(html, /site-root/);
 });
 
-test("client data is empty in the base template", () => {
-  assert.equal(site.master.name, "");
-  assert.equal(site.location.city, "");
-  assert.equal(site.contacts.phoneDisplay, "");
-  assert.equal(site.reviews.length, 0);
-  assert.equal(site.images.gallery.length, 0);
-  assert.equal(Object.values(site.services).flat().length, 0);
-});
-
 test("the clean template uses one canonical stylesheet and runtime", () => {
   assert.ok(fs.existsSync("app/template.css"));
   assert.ok(fs.existsSync("public/template-runtime.js"));
